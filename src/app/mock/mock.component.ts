f38f0760e1acb910758b52820c9ddceb209268e0
Zivojin Djordjevic <zivojin.90@gmail.com>
2018-05-11 04:24:18 +0200

    Minor homepage and detail page fixes

--- src/app/mock/mock.component.ts
+++ src/app/mock/mock.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mock',
-  templateUrl: 'html/product.html',
+  templateUrl: 'html/homepage.html',
   styleUrls: ['./mock.component.scss']
 })
 export class MockComponent implements OnInit {
